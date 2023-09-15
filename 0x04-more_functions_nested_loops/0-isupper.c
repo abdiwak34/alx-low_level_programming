9501be4f08a50ba6d5153b96320fbb39a0a389b9
abdiwak34 <abdiwakamsalu@gmail.com>
2023-09-15 11:56:19 -0700

    more func

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase
+ * @c: to be checked
+ * return: 1 if it is uppper else 0
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+more fun and more nested loop
