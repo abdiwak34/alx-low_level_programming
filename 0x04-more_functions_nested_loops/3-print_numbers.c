74a276d92b091adb5e27b798abe8ddade79f1a20
abdiwak34 <abdiwakamsalu@gmail.com>
2023-09-15 13:02:38 -0700

    print nos

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints no from 0 upto 9
+ * return: none
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	while (i < 10)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar(\n);
+}
