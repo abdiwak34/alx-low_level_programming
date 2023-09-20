69e509eecb3974bcebeb0f6d03ec82427f256a84
abdiwak34 <abdiwakamsalu@gmail.com>
2023-09-20 10:44:09 -0700

    connecting string

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - connecting two string
+ * @dest: arg 1
+ * @src: arg 2
+ * @n: bytes
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	_strncat(dest, src);
+	return (dest);
+}
