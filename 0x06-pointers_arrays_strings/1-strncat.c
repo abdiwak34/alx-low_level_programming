3497a4d1f09f3dd067feb751b4ce14d6889c46d7
abdiwak amsalu <abdiwakamsalu@gmail.com>
2023-09-21 10:22:09 -0700

    connecting string

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,16 +1,31 @@
 #include "main.h"
-#include <string.h>
 
 /**
- * _strncat - connecting two string
- * @dest: arg 1
- * @src: arg 2
- * @n: bytes
- *Return: dest
- */
-
+ *  * _strncat - concatenates n bytes from a string to another
+ *   * @dest: destination string
+ *    * @src: source string
+ *     * @n: number of bytes of str to concatenate
+ *      *
+ *       * Return: a pointer to the resulting string dest
+ *        */
 char *_strncat(char *dest, char *src, int n)
 {
-	_strncat(dest, src);
-	return (dest);
+		int i, j;
+
+			i = 0;
+				j = 0;
+
+					while (dest[i] != '\0')
+								i++;
+
+						while (src[j] != '\0' && j < n)
+								{
+											dest[i] = src[j];
+													i++;
+															j++;
+																}
+
+							dest[i] = '\0';
+
+								return (dest);
 }
