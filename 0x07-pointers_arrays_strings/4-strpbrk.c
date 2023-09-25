f9f4c597620f8535b281b44d9deba933e5e4a278
abdiwak amsalu <abdiwakamsalu@gmail.com>
2023-09-25 06:02:32 -0700

    searches a string

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strpbrk - search a string
+ * @s: msin string
+ * @accept: string to be searched
+ * Return: pointer
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	strpbrk(const char *str, const char *charset);
+}
