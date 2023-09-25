ee3241c863ff8562142ed36e8d0eab0447851e47
abdiwak amsalu <abdiwakamsalu@gmail.com>
2023-09-25 13:28:54 -0700

    print the chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints 8*8 cheese
+ * @a: the arr
+ * prints the chess
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i,j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			printf("%c ", a[i][j]);
+		}
+	}
+}
