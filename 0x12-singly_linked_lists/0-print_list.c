7ba282db68d23e75c3824df25301e9075391065c
Abdiwak Amsalu <abdiwakamsalu@gmail.com>
2023-10-19 12:12:40 -0700

    print the list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,9 +1,12 @@
+#include <stdio.h>
 #include "main.h"
+
 /**
  * print_list - prints the list of the singly linked list
  *
  * @*h: the pointer to the next node
- * return: thelength of the node
+ *
+ * Return: the number of the node
  */
 
 size_t print_list(const list_t *h)
@@ -22,4 +25,3 @@ size_t print_list(const list_t *h)
 
 	return (s);
 }
-
