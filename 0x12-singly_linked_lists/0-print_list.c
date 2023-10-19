6a29ae4cdab81d15bea1898bbc4e67f83af5f8a3
Abdiwak Amsalu <abdiwakamsalu@gmail.com>
2023-10-19 09:29:04 -0700

    linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_list - prints the list of the singly linked list
+ *
+ * @*h: the pointer to the next node
+ * return: thelength of the node
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t s = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		s++;
+	}
+
+	return (s);
+}
+
