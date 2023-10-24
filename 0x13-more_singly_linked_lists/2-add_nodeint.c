9bd27064dc4d854a30cb763b0cabfb92546e3e58
Abdiwak Amsalu <abdiwakamsalu@gmail.com>
2023-10-24 12:07:28 -0700

    update on add node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -4,14 +4,20 @@
  * add_ndeint - add a node to the first node
  * @head: the pointer to the first head
  * @n: the data of added node
+ * Return: new_node will be returned
  */
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	struct list_t *new_node = (struct list_t*) malloc(sizeof(stuct list_t));
-	new_node -> data = n;
-	new_node -> next = head;
-	head = new_node;
+	listint_t *new_node;
 
-	return(head);
+	 new_node = malloc(sizeof(stuct list_t));
+	if(!new_node)
+		return(NULL);
+
+	new_node -> n = n;
+	new_node -> next = *head;
+	*head = new_node;
+
+	return(new_node);
 }
